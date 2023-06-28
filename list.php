ac18b75d281c5e7da3243263a68f19693d3512f4
Ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2023-06-28 18:12:21 +0330

    create list page

---
+++ list.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>To-do</title>
+
+    <link rel="stylesheet" href="./bootstrap-5.1.3-dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="style.css" />
+
+
+    <style>
+      body {
+        background-color: wheat;
+      }
+
+      .task-container {
+        margin-top: 2.5em;
+        margin-left: 25%;
+      }
+
+      #inputTask {
+        margin-top: 0.1em;
+        border: black dashed 0.1em;
+      }
+
+      .add-btn {
+        margin-top: 0.1em;
+      }
+
+      table {
+        width: 50% !important;
+        margin-top: 2em;
+        margin-left: 25%;
+      }
+    </style>
+    <?php
+      include('navbar.php');
+    ?>
+  </head>
+  <body>
+
+  <?php 
+      include_once('connection.php');
+      $con = mysqli_connect('localhost', 'root', '', 'todo_db');
+      $results = mysqli_query($conn, "SELECT * FROM tasks");
+    ?>
+    <table style="margin-top: 100px;">
+      <thead>
+        <tr>
+          <th style="font-size: 25px; text-align: left;" colspan="3">My tasks</th>
+        </tr>
+      </thead>
+
+      <tr>
+        <th>#</th>
+        <th>Task name</th>
+        <th>Deadline</th>
+        <th>Priority</th>
+        <!-- <th colspan="2">Options</th> -->
+      </tr>
+      
+      <?php 
+        print_r($results);
+        $i = 1;
+        if(!$results){
+          die(mysqli_error($con));
+        }
+        if(mysqli_num_rows($results)){
+          while ($row = mysqli_fetch_array($results)) { ?>
+            <tr>
+              <td><?php echo $i; ?></td>
+              <td><?php echo $row['task_name']; ?></td>
+              <td><?php echo $row['deadline']; ?></td>
+              <td><?php echo $row['priority']; ?></td>
+              <!-- <td>
+                <a href="create_book.php?edit=<?php echo $row['id']; ?>&action=2" class="edit_btn">Edit</a>
+                <a href="change_db.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+              </td> -->
+            </tr>
+            <?php $i++;}
+        }
+      ?>
+    </table>		
+
+    <script>
+      var my_table = document.getElementById("my_table");
+      var my_list = document.getElementsByTagName("TR");
+
+      function add_task() {
+        let new_index = my_list.length;
+        const input_task = document.getElementById("inputTask").value;
+        var body = document.getElementById("table_body");
+        var new_tr =
+          '<tr><td scope="col">' +
+          new_index +
+          '</td><td scope="col">' +
+          input_task +
+          '</td><td scope="col"><button id="' +
+          new_index +
+          '" class="btn btn-outline-primary" onclick="remove_task(this.id)">Done</button></td>';
+        body.innerHTML += new_tr;
+        document.getElementById("inputCol").innerHTML =
+          '<input id="inputTask" class="form-control" placeholder="New task" />';
+      }
+
+      function remove_task(index) {
+        my_table.deleteRow(index);
+        for (let i = 1; i < my_list.length; i++) {
+          my_table.rows[i].cells[0].innerHTML = i;
+        }
+      }
+    </script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,49 @@
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  
+  <style>
+    body {
+      background-color: #fbfbfb;
+    }
+    @media (min-width: 991.98px) {
+      main {
+        padding-left: 240px;
+      }
+    }
+
+  </style>
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <!-- Sidebar -->
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('i a').click(function(){
+        $('i a').removeClass("active");
+        $(this).addClass("active");
+      });
+    });
+  </script>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="login.php" style="font-size:16px">Logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="home.php" style="font-size:16px">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="list.php" style="font-size:16px">My list</a>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+
+</head>
+
+
