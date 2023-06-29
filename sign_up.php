3fe2fcad5e1cd5ccf3670c4e21543fa5bcf137b2
Ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2023-06-29 20:34:50 +0330

    change login & signup css

--- sign_up.php
+++ sign_up.php
@@ -6,9 +6,73 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="login.css">
+    <!-- <link rel="stylesheet" href="login.css"> -->
     <title>Sign Up</title>
 </head>
+
+<style>
+    
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: url() no-repeat;
+  background-size: cover;
+}
+
+.login-box {
+  width: 280px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #343a40;
+}
+
+.login-box h1 {
+  float: left;
+  font-size: 40px;
+  border-bottom: 4px solid #343a40;
+  margin-bottom: 50px;
+  padding: 13px;
+}
+
+.textbox {
+  width: 100%;
+  overflow: hidden;
+  font-size: 20px;
+  padding: 8px 0;
+  margin: 8px 0;
+  border-bottom: 1px solid #343a40;
+}
+
+.fa {
+  width: px;
+  float: left;
+  text-align: center;
+}
+
+.textbox input {
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 18px;
+  float: left;
+  margin: 0 10px;
+}
+
+.button {
+  width: 100%;
+  padding: 8px;
+  color: #ffffff;
+  background: none #343a40;
+  border: none;
+  border-radius: 6px;
+  font-size: 18px;
+  cursor: pointer;
+  margin: 12px 0;
+}
+</style>
 
 <?php
     session_start();
@@ -17,7 +81,7 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $username = validate($_POST['username']);
         $password = validate($_POST['password']);
-        if (empty($username)|| empty($password)) {
+        if (empty($username) or empty($password)) {
             echo "Please fill out all required fields.";
         }else {
                 $sql = "INSERT INTO `users`(`username`, `password`) VALUES ('$username', '$password')";
@@ -26,6 +90,10 @@
                     echo "<SCRIPT>
                     alert('New user saved!')
                     window.location.replace('http://localhost/Todolist/login.php');
+                    </SCRIPT>";
+                }else{
+                    echo "<SCRIPT>
+                    alert('This username already exist! Try again with another username.')
                     </SCRIPT>";
                 }
         }
